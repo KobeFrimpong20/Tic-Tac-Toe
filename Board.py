acc7f3b601b9e448245f2ea98ceba1191320e32f
Kobe <kobe_s_frimpong@yahoo.com>
2020-12-25 21:24:39 -0500

    adding Board file

---
+++ Board.py
@@ -0,0 +1,5 @@
+class Board:
+
+	side_length = 3
+	side_length = int(input('Enter the desired length for the board: '))
+	print('The game board will be a ' + str(side_length) + 'x' + str(side_length))
