7c68174f9a91daa639a7990b1c3d46bbda5acfb8
Kobe <kobe_s_frimpong@yahoo.com>
2020-12-26 11:43:04 -0500

    added all files

--- Board.py
+++ Board.py
@@ -1,5 +1,41 @@
 class Board:
+	"""Represents A single game board
 
-	side_length = 3
-	side_length = int(input('Enter the desired length for the board: '))
-	print('The game board will be a ' + str(side_length) + 'x' + str(side_length))
+	Attributes
+	___________
+
+	side_length : int
+		the length of each side of the board
+	board : 2D list of strings
+		represents the current version of the square game board
+
+	Methods
+	________
+
+	draw_board()
+		draws the current version of the board
+	update_board()
+		updates the board to the most recent move"""
+
+	def __init__(self, side_length):
+		"""creates a new instance of the game board """
+		self.side_length = side_length
+		self.board = [['-' for n in range(self.side_length)] for m in range(self.side_length)]
+
+	def draw_board(self):
+		"""draws the current version of the board"""
+		print('The game board will be a ' + str(self.side_length) + 'x' + str(self.side_length))
+		for row in range(self.side_length):
+			current_line = ''
+			for col in range(self.side_length):
+				current_line += self.board[row][col] + '\t'
+			print(current_line.strip())
+
+	def update_board(self, new_board):
+		for row in range(self.side_length):
+			for col in range(self.side_length):
+				self.board[row][col] = new_board[row][col]
+
+if __name__ == '__main__':
+	game_board = Board(3)
+	game_board.draw_board()
